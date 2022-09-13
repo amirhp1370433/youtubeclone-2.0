04002761646f854f86499d15fdad9a87c9d9f671
amirhp1370433 <amirhp1370433@gmail.com>
2022-09-13 10:43:44 +0430

    ok

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Videos = () => {
+const Videos = ({videos}) => {
+  console.log(videos)
   return (
-    <div>Videos</div>
+    <div>
+      
+    </div>
   )
 }
 
