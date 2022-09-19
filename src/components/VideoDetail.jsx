d09b295eecde62ec24eebb7cdbfb1b478da4df08
amirhp1370433 <amirhp1370433@gmail.com>
2022-09-19 12:45:44 +0330

    ok

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
-
+import { useState , useEffect } from 'react'
+import {Link , useParams} from 'react-router-dom'
+import ReactPlayer from 'react-player'
+import {Box , Stack , Typography} from '@mui/material'
+import {CheckCircle} from '@mui/icons-material'
+import {video} from './'
+import { fetchFromAPI } from '../utils/fetchFromAPI'
 const VideoDetail = () => {
+  const [first, setfirst] = useState(second)
+  useEffect(() => {
+
+  }, [])
+  
   return (
     <div>VideoDetail</div>
   )
