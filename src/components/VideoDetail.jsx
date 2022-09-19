8381358b8a617e0eb38f148718b388cc2dbe53be
amirhp1370433 <amirhp1370433@gmail.com>
2022-09-19 14:02:18 +0330

    video detail

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -7,13 +7,31 @@ import {CheckCircle} from '@mui/icons-material'
 import {video} from './'
 import { fetchFromAPI } from '../utils/fetchFromAPI'
 const VideoDetail = () => {
-  const [first, setfirst] = useState(second)
-  useEffect(() => {
+const [videoDetail, setVideoDetail] = useState('')
+const {id} = useParams()
 
-  }, [])
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet?statistics&id=${id}`)
+    .then((data)=>setVideoDetail(data.items[0])) 
+  }, [id ])
+  // check that video detail is null or not
+  if(!videoDetail?.snippet) return 'loading...'
   
+  const {snippet : { title , channelId , channelTitle} , statistics:{videoCount , likeCount }} = videoDetail
   return (
-    <div>VideoDetail</div>
+    <Box minHeight='95vh'>
+      <Stack direction={{xs:'column' , md:'row'}}>
+        <Box flex={1}>
+          <Box sx={{width:"100%" , position:'sticky' , top:'86px' }}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls/>
+            <Typography color="#fff" variant="h5" fontWeight='bold' p={2}>
+              {title}
+            </Typography>
+          
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
   )
 }
 
